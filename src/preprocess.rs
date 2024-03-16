7c5adb01a375fb8adb44fef2e269cda035f0cb7f
rogeryoungh <rogeryoungh@foxmail.com>
2024-03-16 17:00:53 +0800

    update KATEX_HEADER to match katex-rs

--- src/preprocess.rs
+++ src/preprocess.rs
@@ -19,7 +19,7 @@ use crate::{
 };
 
 /// Header that points to CDN for the KaTeX stylesheet.
-pub const KATEX_HEADER: &str = r#"<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossorigin="anonymous">
+pub const KATEX_HEADER: &str = r#"<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css">
 
 "#;
 
